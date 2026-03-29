93298b49d04ef765662cf538f64ad5943fe016a4
starsvoyage <idiazvachier@arizona.edu>
2026-03-29 16:47:46 -0700

    Add account reset and lockeout features.

---
+++ src/main/java/edu/arizona/videoshare/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,13 @@
+package edu.arizona.videoshare.repository;
+
+import edu.arizona.videoshare.model.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+
+    void deleteByUserId(Long userId);
+}
