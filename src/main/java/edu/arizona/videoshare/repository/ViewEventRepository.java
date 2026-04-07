b116152366258f47177572a56b328d85a5c67b2e
unknown <iammichaellucero@gmail.com>
2026-04-07 14:21:58 -0700

    Update watch history to refresh existing video entries

--- src/main/java/edu/arizona/videoshare/repository/ViewEventRepository.java
+++ src/main/java/edu/arizona/videoshare/repository/ViewEventRepository.java
@@ -1,13 +1,16 @@
 package edu.arizona.videoshare.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.arizona.videoshare.model.entity.ViewEvent;
 
-public interface ViewEventRepository extends JpaRepository<ViewEvent, Long>{
-    List<ViewEvent> findByUserId(Long userId);
-    
+public interface ViewEventRepository extends JpaRepository<ViewEvent, Long> {
+    List<ViewEvent> findByUserIdOrderByWatchedAtDesc(Long userId);
+
     List<ViewEvent> findByVideoId(Long videoId);
+
+    Optional<ViewEvent> findByUserIdAndVideoId(Long userId, Long videoId);
 }
